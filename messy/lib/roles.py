b644a8c6c3b72e2fefc62fae260bc8ba0dc6ee3a
anto_at_prodesk <anto@eijkman.go.id>
2021-06-11 05:53:51 +0700

    add manager roles

--- messy/lib/roles.py
+++ messy/lib/roles.py
@@ -1,19 +1,25 @@
 
 from rhombus.lib.roles import *
 
+INSTITUTION_MANAGE = '~r|institution|manage'
 INSTITUTION_MODIFY = '~r|institution|modify'
 
+COLLECTION_MANAGE = '~r|collection|manage'
 COLLECTION_MODIFY = '~r|collection|modify'
 COLLECTION_VIEW = '~r|collection|view'
 
+SAMPLE_MANAGE = '~r|sample|manage'
 SAMPLE_MODIFY = '~r|sample|modify'
 SAMPLE_VIEW = '~r|sample|view'
 
+PLATE_MANAGE = '~r|plate|manage'
 PLATE_MODIFY = '~r|plate|modify'
 PLATE_VIEW = '~r|plate|view'
 
+SEQUENCINGRUN_MANAGE = '~r|sequencingrun|manage'
 SEQUENCINGRUN_MODIFY = '~r|sequencingrun|modify'
 SEQUENCINGRUN_VIEW = '~r|sequencingrun|view'
 
+SEQUENCE_MANAGE = '~r|sequence|manage'
 SEQUENCE_MODIFY = '~r|sequence|modify'
 SEQUENCE_VIEW = '~r|sequence|view'
