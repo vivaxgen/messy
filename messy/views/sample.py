c99e241b156cdd692137bfcb7d812fc2b8b89420
anto_at_prodesk <anto@eijkman.go.id>
2021-06-12 18:47:51 +0700

    use form_fields mechanism; add age to listing

--- messy/views/sample.py
+++ messy/views/sample.py
@@ -12,6 +12,31 @@ class SampleViewer(BaseViewer):
     edit_route = 'messy.sample-edit'
     view_route = 'messy.sample-view'
 
+    form_fields = {
+        'collection_id': ('messy-sample-collection_id', ),
+        'code': ('messy-sample-code', ),
+        'lab_code': ('messy-sample-lab_code', ),
+        'category_id': ('messy-sample-category_id', ),
+        'sequence_name': ('messy-sample-sequence_name', ),
+        'location': ('messy-sample-location', ),
+        'location_info': ('messy-sample-location_info', ),
+        'collection_date': ('messy-sample-collection_date', ),
+        'received_date': ('messy-sample-received_date', ),
+        'specimen_type_id': ('messy-sample-specimen_type_id', ),
+        'passage_id': ('messy-sample-passage_id', ),
+        'species_id': ('messy-sample-species_id', ),
+        'host_id': ('messy-sample-host_id', ),
+        'host_info': ('messy-sample-host_info', ),
+        'host_gender': ('messy-sample-host_gender', ),
+        'host_age': ('messy-sample-host_age', float),
+        'host_status_id': ('messy-sample-host_status_id', ),
+        'host_severity': ('messy-sample-host_severity', int),
+        'host_occupation_id': ('messy-sample-host_occupation_id', ),
+        'originating_institution_id': ('messy-sample-originating_institution_id', ),
+        'originating_code': ('messy-sample-originating_code', ),
+        'sampling_institution_id': ('messy-sample-sampling_institution_id', ),
+        'sampling_code': ('messy-sample-sampling_code', ),
+    }
 
     @m_roles( PUBLIC )
     def index(self):
@@ -35,7 +60,6 @@ class SampleViewer(BaseViewer):
         try:
             obj.update(d)
             # check if obj is already registered
-            raise
             if obj.id:
                 # check whether users is in sample collection group
                 group = obj.collection.group
@@ -45,22 +69,6 @@ class SampleViewer(BaseViewer):
         except RuntimeError as err:
             raise
 
-
-    def parse_form(self, f, d=None):
-        d = super().parse_form(f, d,
-            [
-                ('collection_id', 'messy-sample-collection_id'),
-                ('code', 'messy-sample-code'),
-                ('lab_code', 'messy-sample-lab_code'),
-                ('sequence_name', 'messy-sample-sequence_name'),
-                ('location', 'messy-sample-location'),
-                ('location_info', 'messy-sample-location_info'),
-                ('collection_date', 'messy-sample-collection_date'),
-                ('passage', 'messy-sample-passage'),
-            ]
-        )
-
-        return d
 
     def edit_form(self, obj=None, create=False, readonly=False, update_dict=None):
 
@@ -76,6 +84,8 @@ class SampleViewer(BaseViewer):
             samp_inst = dbh.get_institutions_by_ids(
                         [update_dict['messy-sample-sampling_institution_id']], None)[0]
 
+        ff = self.form_fields
+
         eform = form( name='messy-sample', method=POST)[
             self.hidden_fields(obj),
             fieldset(
@@ -85,29 +95,51 @@ class SampleViewer(BaseViewer):
                             groups = None if req.user.has_roles( SYSADM, DATAADM ) else req.user.groups
                         ) ],
                     static=readonly),
-                input_text('messy-sample-code', 'Code', value=obj.code,
+                input_text(ff['code'][0], 'Code', value=obj.code,
                     offset=2, static=readonly, update_dict=update_dict),
-                input_text('messy-sample-lab_code', 'Lab Code', value=obj.lab_code,
+                input_text(ff['lab_code'][0], 'Lab Code', value=obj.lab_code,
                     offset=2, static=readonly, update_dict=update_dict),
-                input_text('messy-sample-sequence_name', 'Sequence name', value=obj.sequence_name,
+                input_text(ff['received_date'][0], 'Received Date', value=obj.received_date,
                     offset=2, static=readonly, update_dict=update_dict),
-                input_text('messy-sample-location', 'Location', value=obj.location,
+                input_select_ek(ff['category_id'][0], 'Category',
+                    value = obj.category_id, offset=2, size=5, static=readonly,
+                    parent_ek = dbh.get_ekey('@CATEGORY')),
+
+                input_text(ff['location'][0], 'Location', value=obj.location,
                     offset=2, static=readonly, update_dict=update_dict),
-                input_text('messy-sample-location_info', 'Additional location', value=obj.location_info,
+                input_text(ff['location_info'][0], 'Additional location', value=obj.location_info,
                     offset=2, static=readonly, update_dict=update_dict),
-                input_text('messy-sample-collection_date', 'Collection date', value=obj.collection_date,
+                input_text(ff['collection_date'][0], 'Collection date', value=obj.collection_date,
                     offset=2, static=readonly, update_dict=update_dict),
-                input_select('messy-sample-originating_institution_id', 'Originating Institution',
+                input_text(ff['sequence_name'][0], 'Sequence name', value=obj.sequence_name,
+                    offset=2, static=readonly, update_dict=update_dict),
+
+                input_text(ff['host_age'][0], 'Host Age', value=obj.host_age,
+                    offset=2, static=readonly,  update_dict=update_dict),
+                input_select_ek(ff['host_status_id'][0], 'Host Status',
+                    value = obj.host_status_id, offset=2, size=5, static=readonly,
+                    parent_ek = dbh.get_ekey('@CATEGORY')),
+                input_text(ff['host_severity'][0], 'Host Severity', value=obj.host_severity,
+                    offset=2, static=readonly, update_dict=update_dict),
+                input_select_ek(ff['host_occupation_id'][0], 'Host Occupation',
+                    value = obj.host_occupation_id, offset=2, size=5, static=readonly,
+                    parent_ek = dbh.get_ekey('@HOST_OCCUPATION')),
+
+                input_select_ek(ff['passage_id'][0], 'Passage',
+                    value = obj.passage_id, offset=2, size=5, static=readonly,
+                    parent_ek = dbh.get_ekey('@PASSAGE')),
+
+                input_select(ff['originating_institution_id'][0], 'Originating Institution',
                     value = orig_inst.id if orig_inst else None, offset=2, size=5, static=readonly,
                     options = [ (orig_inst.id, f'{orig_inst.code} | {orig_inst.name}') ] if orig_inst else [],
                     update_dict=update_dict ),
-                input_text('messy-sample-originating_code', 'Originating Code', value=obj.originating_code,
+                input_text(ff['originating_code'][0], 'Originating Code', value=obj.originating_code,
                     offset=2, static=readonly, update_dict=update_dict),
-                input_select('messy-sample-sampling_institution_id', 'Sampling Institution',
+                input_select(ff['sampling_institution_id'][0], 'Sampling Institution',
                     value = samp_inst.id if samp_inst else None, offset=2, size=5, static=readonly,
                     options = [ (samp_inst.id, f'{samp_inst.code} | {samp_inst.name}')] if samp_inst else [],
                     update_dict=update_dict ),
-                input_text('messy-sample-sampling_code', 'Sampling Code', value=obj.sampling_code,
+                input_text(ff['sampling_code'][0], 'Sampling Code', value=obj.sampling_code,
                     offset=2, static=readonly, update_dict=update_dict),
                 name='messy-sample-fieldset'
             ),
@@ -150,6 +182,7 @@ def generate_sample_table(samples, request):
                 td( sample.sequence_name),
                 td( sample.location),
                 td( sample.collection_date),
+                td( f'{sample.host_age:4.1f}' ),
             )
         )
 
@@ -164,6 +197,7 @@ def generate_sample_table(samples, request):
                 th('Name'),
                 th('Location'),
                 th('Collection Date'),
+                th('Age'),
             )
         )
     ]
