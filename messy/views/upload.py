25275e0794f1c6530058b763dd08a33e5346e061
anto_at_prodesk <anto@eijkman.go.id>
2021-08-03 08:42:11 +0700

    reformat to pep8 style

--- messy/views/upload.py
+++ messy/views/upload.py
@@ -1,18 +1,23 @@
 
-from messy.views import *
+from messy.views import (m_roles, get_dbhandler, error_page, render_to_response, HTTPFound,
+                         custom_submit_bar)
 from messy.views.rpc import generate_user_token
 from messy.lib import uploads
+from messy.lib import roles as r
 from rhombus.views.generics import forwarding_page
+from rhombus.lib import tags_b46 as t
 from rhombus.lib.utils import random_string
 import joblib
 import pandas as pd
 import os
+
 
 # tokens
 
 def get_user(token):
     """ return user associated with token """
     pass
+
 
 def get_token(user):
     """ generate a string token to be associated with user """
@@ -25,7 +30,7 @@ class UploadViewer(object):
         self.request = request
         self.dbh = get_dbhandler()
 
-    @m_roles( PUBLIC )
+    @m_roles(r.PUBLIC)
     def index(self):
 
         request = self.request
@@ -54,10 +59,10 @@ class UploadViewer(object):
 
             return error_page(self.request, 'Invalid request!')
 
-        html = div(h2('Upload'))
+        html = t.div(t.h2('Upload'))
 
         eform, jscode = generate_token_form(request)
-        html.add(eform)     
+        html.add(eform)
 
         eform, jscode = sample_upload_form(request)
         html.add(eform)
@@ -71,12 +76,10 @@ class UploadViewer(object):
         jscode = jscode + jscode_
 
         return render_to_response("messy:templates/generic_page.mako",
-                {   'html': html,
-                    'code': jscode,
-                },
-                request = request)
+                                  {'html': html, 'code': jscode},
+                                  request=request)
 
-    @m_roles( PUBLIC )
+    @m_roles(r.PUBLIC)
     def commit(self):
 
         if self.request.method == 'POST':
@@ -97,26 +100,24 @@ class UploadViewer(object):
 
         return error_page(self.request, 'Nothing to be done anymore!')
 
-
     def generate_token(self):
 
         if self.request.method == 'POST':
 
             token = generate_user_token(self.request)
-            html = div(h2('User Token'))[
-                div('Token: ' + token),
+            html = t.div(t.h2('User Token'))[
+                t.div('Token: ' + token),
             ]
             return render_to_response('messy:templates/generic_page.mako',
-                    {   'html': html
-                    }, request=self.request)
+                                      {'html': html},
+                                      request=self.request)
 
         return error_page(self)
 
-
-    def confirmpage_sample(self, job = None):
+    def confirmpage_sample(self, job=None):
 
         if self.request.method == 'POST':
-            collection_id = int( self.request.params.get('messy-sample/collection_id', 0))
+            collection_id = int(self.request.params.get('messy-sample/collection_id', 0))
             collection = self.dbh.get_collections_by_ids([collection_id], None)[0]
             if not collection.can_upload(self.request.user):
                 raise RuntimeError('Current user cannot upload sample to this collection')
@@ -124,9 +125,11 @@ class UploadViewer(object):
             infile = self.request.params.get('messy-sample/infile', None)
             gisaid = self.request.params.get('messy-sample/gisaidfile', None)
             if infile is not None and infile != b'':
-                job = uploads.SampleUploadJob( self.request.user.id, infile.filename, infile.file, collection_id )
+                job = uploads.SampleUploadJob(self.request.user.id, infile.filename, infile.file,
+                                              collection_id)
             elif gisaid is not None and gisaid != b'':
-                job = uploads.SampleGISAIDUploadJob( self.request.user.id, gisaid.filename, gisaid.file, collection_id)
+                job = uploads.SampleGISAIDUploadJob(self.request.user.id, gisaid.filename, gisaid.file,
+                                                    collection_id)
             else:
                 return error_page(self.request, 'Please provide either sample file or GISAID file')
             randkey = job.randkey
@@ -134,73 +137,70 @@ class UploadViewer(object):
             joblib.dump(job, job_filename)
 
             return HTTPFound(
-                location = self.request.route_url('upload', _query = { 'jobid': randkey}))
+                location=self.request.route_url('upload', _query={'jobid': randkey}))
 
         if not job:
             return error_page(self.request, 'Invalid request!')
 
         params = job.confirm()
-        html = div() [
-            h3('Sample Upload Confirmation'),
-            div( f"No of samples: {params['samples']}" ),
-            div( f"Error messages: {len(params['err_msgs'])}" ),
+        html = t.div()[
+            t.h3('Sample Upload Confirmation'),
+            t.div(f"No of samples: {params['samples']}"),
+            t.div(f"Error messages: {len(params['err_msgs'])}"),
         ]
         html.add(
-            form('messy/sample', method='POST', action=self.request.route_url('upload-commit')).add(
-                input_hidden('jobid', value=job.randkey),
-                custom_submit_bar(
+            t.form('messy/sample', method='POST', action=self.request.route_url('upload-commit')).add(
+                t.input_hidden('jobid', value=job.randkey),
+                t.custom_submit_bar(
                     ('Add new item only', 'add'),
                     ('Update existing only', 'update'),
                     ('Add new and update existing', 'add_update'),
                 ).set_offset(1).show_reset_button(False)
             )
         )
-        html[h4('Error messages')].add( * [div(msg) for msg in params['err_msgs']] )
+        html[t.h4('Error messages')].add(* [t.div(msg) for msg in params['err_msgs']])
 
         return render_to_response("messy:templates/generic_page.mako",
-            {   'html': html,
-            },
-            request = self.request)
+                                  {'html': html},
+                                  request=self.request)
 
     def commitpage_sample(self, job):
         method = self.request.POST['_method']
         added, updated = job.commit(method)
-        html = div()[
-            h2('Uploaded Samples'),
-            div(f"Added sample(s): {added}"),
-            div(f"Updated sample(s): {updated}"),
+        html = t.div()[
+            t.h2('Uploaded Samples'),
+            t.div(f"Added sample(s): {added}"),
+            t.div(f"Updated sample(s): {updated}"),
         ]
         return render_to_response("messy:templates/generic_page.mako",
-                {   'html': html,
-                },
-                request = self.request)
+                                  {'html': html, },
+                                  request=self.request)
 
+    def confirmpage_institution(self, job=None):
 
-    def confirmpage_institution(self, job = None):
-        
         if self.request.method == 'POST':
             file_content = self.request.params.get('messy-institution/infile')
-            job = uploads.InstitutionUploadJob( self.request.user.id, file_content.filename, file_content.file )
+            job = uploads.InstitutionUploadJob(self.request.user.id, file_content.filename, file_content.file)
             randkey = job.randkey
             job_filename = uploads.create_temp_file('upload', self.request.user.login, 'joblib', randkey)
             joblib.dump(job, job_filename)
 
             return HTTPFound(
-                location = self.request.route_url('upload', _query = { 'jobid': randkey}))
+                location=self.request.route_url('upload', _query={'jobid': randkey}))
 
         if not job:
             return error_page(self.request, 'Invalid request!')
 
         params = job.confirm()
-        html = div() [
-            h3('Institution Upload Confirmation'),
-            div(f"Existing item(s): {params['existing']}"),
-            div(f"New item(s): {params['new']}"),
+        html = t.div()[
+            t.h3('Institution Upload Confirmation'),
+            t.div(f"Existing item(s): {params['existing']}"),
+            t.div(f"New item(s): {params['new']}"),
         ]
         html.add(
-            form('messy/institution', method='POST', action=self.request.route_url('upload-commit')).add(
-                input_hidden('jobid', value=job.randkey),
-                custom_submit_bar(
+            t.form('messy/institution', method='POST', action=self.request.route_url('upload-commit')).add(
+                t.input_hidden('jobid', value=job.randkey),
+                t.custom_submit_bar(
                     ('Add new item only', 'add'),
                     ('Update existing only', 'update'),
                     ('Add new and update existing', 'add_update'),
@@ -209,33 +209,30 @@ class UploadViewer(object):
         )
 
         return render_to_response("messy:templates/generic_page.mako",
-            {   'html': html,
-            },
-            request = self.request)
-
+                                  {'html': html, },
+                                  request=self.request)
 
     def commitpage_institution(self, job):
 
         method = self.request.POST['_method']
         added, updated = job.commit(method)
-        html = div()[
-            h2('Uploaded Institution'),
-            div(f"Added institution(s): {added}"),
-            div(f"Updated institution(s): {updated}"),
+        html = t.div()[
+            t.h2('Uploaded Institution'),
+            t.div(f"Added institution(s): {added}"),
+            t.div(f"Updated institution(s): {updated}"),
         ]
         return render_to_response("messy:templates/generic_page.mako",
-                {   'html': html,
-                },
-                request = self.request)
+                                  {'html': html, },
+                                  request=self.request)
 
 
 def generate_token_form(request):
 
-    html = div(HR, h3('Token Generator', styles="bg-dark;"))
-    eform = form( name='messy/token-generator', method=POST)
+    html = t.div(t.hr, t.h3('Token Generator', styles="bg-dark;"))
+    eform = t.form(name='messy/token-generator', method=t.POST)
     eform.add(
-        custom_submit_bar( ('Generate token', 'generate_token') ).set_offset(1).show_reset_button(False)
-        )
+        custom_submit_bar(('Generate token', 'generate_token')).set_offset(1).show_reset_button(False)
+    )
 
     return html.add(eform), ''
 
@@ -244,23 +241,23 @@ def sample_upload_form(request):
 
     dbh = get_dbhandler()
 
-    html = div(HR, h3('Samples'))
-    sampleform = form( name='messy/sample', method=POST, enctype=FORM_MULTIPART)
+    html = t.div(t.hr, t.h3('Samples'))
+    sampleform = t.form(name='messy/sample', method=t.POST, enctype=t.FORM_MULTIPART)
     sampleform.add(
-        fieldset(
-            input_file('messy-sample/infile', 'CSV or JSON/YAML file',
-                offset=2, size=6,
-                info = 'Click <a href="/help/templates/index.rst" target="_blank">here</a>'
-                        ' to see templates.'),
-            div('or', offset=2),
-            input_file('messy-sample/gisaidfile', 'GISAID CSV file',
-                offset=2, size=6,
-                info = 'Click <a href="/help/templates/index.rst" target="_blank">here</a>'
-                        ' to see templates.'),
-            input_select('messy-sample/collection_id', 'Collection',
-                value = None, offset=1, size=2,
-                options = [ (c.id, c.code) for c in dbh.get_collections(groups=None)] ),
-            custom_submit_bar( ('Upload', 'upload_samples') ).set_offset(1),
+        t.fieldset(
+            t.input_file('messy-sample/infile', 'CSV/TSV or JSON/YAML file',
+                         offset=2, size=6,
+                         info='Click <a href="/help/templates/index.rst" target="_blank">here</a>'
+                              ' to see templates.'),
+            t.div('or', offset=2),
+            t.input_file('messy-sample/gisaidfile', 'GISAID CSV file',
+                         offset=2, size=6,
+                         info='Click <a href="/help/templates/index.rst" target="_blank">here</a>'
+                              ' to see templates.'),
+            t.input_select('messy-sample/collection_id', 'Collection',
+                           value=None, offset=1, size=2,
+                           options=[(c.id, c.code) for c in dbh.get_collections(groups=None)]),
+            t.custom_submit_bar(('Upload', 'upload_samples')).set_offset(1),
         )
     )
 
@@ -274,14 +271,14 @@ def plate_upload_form(request):
 
     dbh = get_dbhandler()
 
-    html = div(HR, h3('Plate Layouts'))
-    plateform = form(name='messy/plate', method=POST, enctype=FORM_MULTIPART)[
-        fieldset(
-            input_file('messy-plate/infile', 'CSV or JSON/YAML file',
-                offset=2, size=6,
-                info='Click <a href="/help/templates/index.rst" target="_blank">here</a>'
-                     ' to see templates.'),
-            custom_submit_bar(('Upload', 'upload_plates')).set_offset(1),
+    html = t.div(t.hr, t.h3('Plate Layouts'))
+    plateform = t.form(name='messy/plate', method=t.POST, enctype=t.FORM_MULTIPART)[
+        t.fieldset(
+            t.input_file('messy-plate/infile', 'CSV/TSV or JSON/YAML file',
+                         offset=2, size=6,
+                         info='Click <a href="/help/templates/index.rst" target="_blank">here</a>'
+                         ' to see templates.'),
+            t.custom_submit_bar(('Upload', 'upload_plates')).set_offset(1),
         )
     ]
 
@@ -292,15 +289,15 @@ def institution_upload_form(request):
 
     dbh = get_dbhandler()
 
-    html = div(HR, h3('Institutions'))
-    eform = form( name='messy/institution', method=POST, enctype=FORM_MULTIPART)
+    html = t.div(t.hr, t.h3('Institutions'))
+    eform = t.form(name='messy/institution', method=t.POST, enctype=t.FORM_MULTIPART)
     eform.add(
-        fieldset(
-            input_file('messy-institution/infile', 'CSV or JSON/YAML file',
-                offset=2, size=6,
-                info = 'Click <a href="/help/templates/index.rst" target="_blank">here</a>'
-                        ' to see templates.'),
-            custom_submit_bar( ('Upload', 'upload_institution') ).set_offset(1),
+        t.fieldset(
+            t.input_file('messy-institution/infile', 'CSV/TSV or JSON/YAML file',
+                         offset=2, size=6,
+                         info='Click <a href="/help/templates/index.rst" target="_blank">here</a>'
+                         ' to see templates.'),
+            custom_submit_bar(('Upload', 'upload_institution')).set_offset(1),
         )
     )
 
@@ -318,7 +315,7 @@ def sample_upload(request):
 def institution_upload(request):
 
     file_content = request.params.get('messy-institution/infile')
-    job = uploads.InstitutionUploadJob( file_content.filename, file_content.file )
+    job = uploads.InstitutionUploadJob(file_content.filename, file_content.file)
 
 
     dbh = get_dbhandler()
@@ -327,8 +324,9 @@ def institution_upload(request):
     return True, forwarding_page(request, 'Institution successfully updated', '/', 10)
 
 
-
 upload_functions = {
     'upload_samples': sample_upload,
     'upload_institution': institution_upload,
 }
+
+# EOF
