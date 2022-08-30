f02aa54d1235e53e809f5fee187e6e746ac01664
anto @ aleph <trimarsanto@gmail.com>
2022-08-30 18:16:26 +0700

    pass EK initlist to parent class during init data

--- messy/models/handler.py
+++ messy/models/handler.py
@@ -23,21 +23,9 @@ class MessyQueryConstructor(rhombus_handler.QueryConstructor):
         'sample_id': dbschema.Sample.id,
         'sample_code': dbschema.Sample.code,
         'acc_code': dbschema.Sample.acc_code,
-        'nik': dbschema.Sample.host_nik,
-        'nar': dbschema.Sample.host_nar,
-
-        'sequence': dbschema.Sample.sequence_name,
-        'sequence_id': dbschema.Sequence.id,
-        'accid': dbschema.Sequence.accid,
 
         'plate_id': dbschema.Plate.id,
         'plate_code': dbschema.Plate.code,
-
-        'run_id': dbschema.SequencingRun.id,
-        'run_code': dbschema.SequencingRun.code,
-        'run_serial': dbschema.SequencingRun.serial,
-
-        'runplate_id': dbschema.SequencingRunPlate.id,
     }
 
 
@@ -48,21 +36,20 @@ class DBHandler(rhombus_handler.DBHandler):
     Institution = dbschema.Institution
     Collection = dbschema.Collection
     Sample = dbschema.Sample
-    Sequence = dbschema.Sequence
     Plate = dbschema.Plate
     PlatePosition = dbschema.PlatePosition
-    SequencingRun = dbschema.SequencingRun
-    SequencingRunPlate = dbschema.SequencingRunPlate
 
     query_constructor_class = MessyQueryConstructor
 
-    def initdb(self, create_table=True, init_data=True, rootpasswd=None):
+    def initdb(self, create_table=True, init_data=True, rootpasswd=None, ek_initlist=[]):
         """ initialize database """
-        super().initdb(create_table, init_data, rootpasswd)
+        from .setup import ek_initlist as messy_ek_initlist
+        super().initdb(create_table, init_data, rootpasswd,
+                       ek_initlist=messy_ek_initlist + ek_initlist)
         if init_data:
             from .setup import setup
             setup(self)
-            cerr('[messy-rbmgr] Database has been initialized')
+            cerr('[messy database has been initialized]')
 
     # add additional methods here
 
@@ -156,20 +143,6 @@ class DBHandler(rhombus_handler.DBHandler):
         return self.get_samples(groups, [{'sample_code': codes}], user=user, fetch=fetch, raise_if_empty=raise_if_empty,
                                 ignore_acl=ignore_acl)
 
-    #
-    # Sequences
-
-    def get_sequences(self, groups, specs=None, user=None, fetch=True, raise_if_empty=False):
-
-        q = self.construct_query(self.Sequence, specs)
-        if fetch:
-            q = q.order_by(self.Sequence.id.desc())
-
-        return self.fix_result(q, fetch, raise_if_empty)
-
-    def get_sequences_by_ids(self, ids, groups, user=None, fetch=True, raise_if_empty=False):
-        return self.get_sequences(groups, [{'sequence_id': ids}], user=user, fetch=fetch, raise_if_empty=raise_if_empty)
-
     # Plates
 
     def get_plates(self, groups, specs=None, user=None, fetch=True, raise_if_empty=False, ignore_acl=False):
@@ -191,34 +164,6 @@ class DBHandler(rhombus_handler.DBHandler):
         return self.get_plates(groups, [{'plate_code': codes}], user=user, fetch=fetch,
                                ignore_acl=False, raise_if_empty=raise_if_empty)
 
-    # Runs
 
-    def get_sequencingruns(self, groups, specs=None, user=None, fetch=True, raise_if_empty=False):
-
-        q = self.construct_query(self.SequencingRun, specs)
-        if fetch:
-            q = q.order_by(self.SequencingRun.date.desc())
-
-        return self.fix_result(q, fetch, raise_if_empty)
-
-    def get_sequencingruns_by_ids(self, ids, groups, user=None, fetch=True, raise_if_empty=False):
-        return self.get_sequencingruns(groups, [{'run_id': ids}], user=user, fetch=fetch, raise_if_empty=raise_if_empty)
-
-    def get_sequencingruns_by_codes(self, codes, groups, user=None, fetch=True, raise_if_empty=False):
-        return self.get_sequencingruns(groups, [{'run_code': codes}], user=user, fetch=fetch, raise_if_empty=raise_if_empty)
-
-    # Run plates
-
-    def get_runplates(self, groups, specs=None, user=None, fetch=True, raise_if_empty=False):
-
-        q = self.construct_query(self.SequencingRunPlate, specs)
-        if fetch:
-            q = q.order_by(self.SequencingRunPlate.id)
-
-        return self.fix_result(q, fetch, raise_if_empty)
-
-    def get_runplates_by_ids(self, ids, groups, user=None, fetch=True, raise_if_empty=False):
-        return self.get_runplates(groups, [{'runplate_id': ids}], user=user,
-                                  fetch=fetch, raise_if_empty=raise_if_empty)
 
 # EOF
