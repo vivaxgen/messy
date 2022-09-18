4792252f693313797e7bd78a07299b68bbe45a61
anto @ aleph <trimarsanto@gmail.com>
2022-09-18 23:11:46 +0700

    panelseq extension now initialize ngsmgr

--- messy/ext/panelseq/__init__.py
+++ messy/ext/panelseq/__init__.py
@@ -1,13 +1,19 @@
 
+# messy-panelseq requires messy-ngsmgr to be initialized
+
 from rhombus.lib.utils import cerr, set_dbhandler_class, get_dbhandler_class
 from rhombus.routes import add_route_view, add_route_view_class
 
+from messy.ext import ngsmgr
 from messy.ext.panelseq.models.handler import generate_handler_class
+
 
 set_dbhandler_class(generate_handler_class(get_dbhandler_class()))
 
 
 def includeme(config):
+
+    config.include(ngsmgr.includeme)
 
     add_route_view_class(
         config, 'messy.ext.panelseq.views.panel.PanelViewer', 'messy-panelseq.panel',
