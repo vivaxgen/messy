53641c82ebbf8fc0f9061e6b2268a09f87acadcf
anto @ aleph <trimarsanto@gmail.com>
2023-01-28 12:41:32 +0700

    import not_roles for future usage

--- messy/views/__init__.py
+++ messy/views/__init__.py
@@ -2,7 +2,7 @@
 from rhombus.lib.utils import cerr, cout, random_string, get_dbhandler
 #from rhombus.lib.roles import SYSADM, DATAADM
 from rhombus.views.generics import error_page
-from rhombus.views import (BaseViewer as rb_BaseViewer, roles, m_roles, Response,
+from rhombus.views import (BaseViewer as rb_BaseViewer, roles, m_roles, not_roles, Response,
                            FileIter, HTTPFound, render_to_response, form_submit_bar,
                            fileinstance_to_response, select2_lookup, ParseFormError)
 from rhombus.lib.modals import modal_delete, popup, modal_error
